12f9f19d0d2719262d4f72a8886358cb7b2b1f8f
Alex Test <Wur6un4ik@gmail.com>
2020-10-30 11:57:40 +0200

    Clean Up

--- build/resources/main/static/js/app/ciApp.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var ciApp = angular.module('ciApp', [
-    'ui.bootstrap',
-    'ciApp.controllers',
-    'ciApp.services']);
-
-ciApp.constant('PHONE_NUMBER_PATTERN','^(\\+|00){1}\\d{1,7}\\d{3,}$');
-
